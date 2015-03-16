27e4a484a03bc13b1db965ba76573ffe2a219ea9
novicaa <novicamiketic@gmail.com>
2015-03-16 14:49:45 +0100

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="Robert of Philadelphia Hair Salon at BAYFRONT 440 Bayfront Place, Naples, Florida "; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2392633122;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
